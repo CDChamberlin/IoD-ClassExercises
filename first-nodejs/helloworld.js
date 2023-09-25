1edd39b468bb8780ac6926964e4423cfbb090a99
Collin <cdchamberlin.rva@gmail.com>
2023-09-24 21:07:02 -0400

    Added console logs to call the four functions of the a basic calulator, exercise on slide 84 of module 1

--- first-nodejs/helloworld.js
+++ first-nodejs/helloworld.js
@@ -4,25 +4,35 @@ function SayHello(){
 
 SayHello();
 
+/*
+
+Create 4 functions for the 4 main operations (-,+,/,*). Return the value and output it to the 
+screen. 
+Create a function that takes the name of a person as an argument, and prints out “Hello Name” 
+to the console. Hint search online on how to concatenate two strings
+
+*/
+
 function Sum(A, B){
     return A + B
 }
-
+console.log(Sum(2, 25))
 
 function Subtract(A, B){
    return A - B;
 }
 
+console.log(Subtract(10, 5))
+
 function multiply(A, B){
     return A * B
 }
+console.log(multiply(12, 23))
 
 function division(A, B){
-    if (A > B)
-        return (A/B)
-    else
-        return (B/A)
+     return (A/B)
 }
+console.log(division(20, 4))
 
 function Greet(A){
     return ("Hello " + A)
