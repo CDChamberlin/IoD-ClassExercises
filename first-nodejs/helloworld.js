cd68273f09014a2a6e93eba616e88119e02b58a4
Collin <cdchamberlin.rva@gmail.com>
2023-09-24 21:13:23 -0400

    Working with arrays practice and exercise from slide 116 of module 1

--- first-nodejs/helloworld.js
+++ first-nodejs/helloworld.js
@@ -48,3 +48,41 @@ const sentiment = new Sentiment()
 const result = sentiment.analyze("Cats are clever and stupid.")
 console.dir(result)
 
+/* 
+    Module 1 part 7
+    Algorithms and Data Structures
+*/
+
+let helloText = "say Hi";
+let check = 4;
+if (check>3){
+    let hello = "say Hello instead";
+    console.log(hello);
+}
+//console.log(hello)
+
+let mountains = ["Everest", "Fuji", "Nanga Parbat"]
+
+let seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'], ;
+// console.log(seas);
+seas.push('Red Sea'); // adds to the end of the array
+// console.log(seas);
+// seas.unshift('Blue Sea') // adds to the begining of the array
+
+// const lastElement = seas.pop;
+// const firstElement = seas.shift;
+
+let index = seas.indexOf('North Sea')
+
+/*
+Exercise 
+    • For understanding more about arrays try creating an array that has 5 elements.
+    • Replace the value of the element at position 1 and 4. 
+    • Print the array to the console.
+*/
+
+let oceans = ["Alantic", "Pacific", "Indian", "Artic", "Southern"];
+oceans[1] = "Red Sea";
+oceans[4] = "Black Sea";
+
+console.dir(oceans)
