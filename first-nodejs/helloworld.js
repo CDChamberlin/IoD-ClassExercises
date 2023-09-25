bc594459c8c39ce37561ca4649711fec1f4a169d
Collin <cdchamberlin.rva@gmail.com>
2023-09-24 20:02:01 -0400

    Exercises from Saturday, 9-23

---
+++ first-nodejs/helloworld.js
@@ -0,0 +1,29 @@
+function SayHello(){
+    console.log('Hello World, again.');
+}
+
+SayHello();
+
+function Sum(A, B){
+    return A + B
+}
+
+
+function Subtract(A, B){
+   return A - B;
+}
+
+function multiply(A, B){
+    return A * B
+}
+
+function division(A, B){
+    if (A > B)
+        return (A/B)
+    else
+        return (B/A)
+}
+
+function Greet(A){
+    return ("Hello " + A)
+}
